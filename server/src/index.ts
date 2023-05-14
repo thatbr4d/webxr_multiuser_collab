8e0efa34e541ffc48b8cf03b7a312a665c631605
thatbr4d <thatbr4d@gmail.com>
2023-05-14 11:24:14 -0400

    add server monitor

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { createServer } from "http";
 import { Server } from "@colyseus/core";
+import { monitor } from "@colyseus/monitor";
 import { ARRoom } from "./rooms/ARRoom";
 
 const port = Number(process.env.PORT || 2567);
 const app = express();
+app.use("/servermonitor", monitor());
 
 const arServer = new Server({
     server: createServer(app)

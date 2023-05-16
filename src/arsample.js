57063a4cda5712ea4ee842566e6dbc0c1b5eab17
thatbr4d <thatbr4d@gmail.com>
2023-05-16 17:55:37 -0400

    add the users name next to the model in AR 3d space

--- src/arsample.js
+++ src/arsample.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import { ARButton } from "three/addons/webxr/ARButton.js";
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { Text } from 'troika-three-text';
 
 let scene;
 let camera;
@@ -46,20 +47,34 @@ export function ARAnimate() {
 
 export function AddCollaborator(key, modelIndex, name) {
   console.log("add collab");
-  let geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+  const HEIGHT = .5;
+  const WIDTH = .5;
+
+  let geometry = new THREE.BoxGeometry(0.5, HEIGHT, 0.5);
   let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
   let cube = new THREE.Mesh(geometry, material);
   cube.name = key;
 
-  let collab = { id: key, model: cube, name: name };
+  const myText = new Text();
+  myText.name = "text" + key;
+  myText.text = name;
+  myText.fontSize = .5;
+  myText.position.set(WIDTH / 2, HEIGHT, -9);
+
+  let collab = { id: key, model: cube, name: name, textModel: myText };
   collab.model.position.set(0, 0, -10);
 
   scene.add(collab.model);
+  scene.add(myText);
+
   collaborators.push(collab);
+
+  myText.sync();
 }
 
 export function RemoveCollaborator(key) {
   removeObject3D(scene.getObjectByName(key));
+  removeObject3D(scene.getObjectByName("text" + key));
 
   let collab = collaborators.filter(x => x.id == key);
   collaborators.pop(collab);

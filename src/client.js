831499e927badd5baa52b35b97b25045aff68420
BenBoryla <BenBoryla@Gmail.com>
2023-05-16 07:54:05 -0400

    Display a table or existing rooms to join.

--- src/client.js
+++ src/client.js
@@ -3,7 +3,8 @@ import { AddCollaborator, RemoveCollaborator } from "./arsample";
 
 export default class Client {
   createBtn;
-  joinBtn;
+  joinBtns;
+  backBtn;
 
   constructor() {
     //var client = new Colyseus.Client("wss://webxrcollab.azurewebsites.net:2567");
@@ -13,22 +14,40 @@ export default class Client {
       "CreateJoinRoomWrapper"
     );
 
-    this.joinBtn = document.getElementById("BtnJoinRoom");
+    this.joinBtns = document.getElementsByClassName("btnJoinRoom");
     this.createBtn = document.getElementById("BtnCreateRoom");
+    this.backBtn = document.getElementById("BtnGoBack");
 
-    this.codeInput = document.getElementById("RoomCode");
+    this.codeInput = "";
+    // this.codeInput = document.getElementById("RoomCode");
+    this.goBack = document.getElementById("GoBackWrapper");
     this.roomInfo = document.getElementById("RoomInfo");
+    this.roomCodeDisplay = document.getElementById("RoomCodeDisplay");
     this.userName = document.getElementById("UserName");
   }
 
   get codeInputVal() {
-    return this.codeInput.value;
+    //return this.codeInput.value;
+    return this.codeInput;
+  }
+
+
+
+  async getRooms() {
+    var roomInfo = [];
+    var rooms = await this.client.getAvailableRooms();
+    rooms.forEach((room) => {
+      console.log(rooms);
+      roomInfo.push({ roomId: room.roomId, clients: room.clients });
+    });
+
+    return roomInfo;
   }
 
   async joinRoom(code) {
     if (code !== undefined && code !== "") {
       this.client
-        .joinById(code, {name: this.userName.value})
+        .joinById(code, { name: this.userName.value })
         .then((room) => {
           console.log(room);
 
@@ -41,7 +60,7 @@ export default class Client {
       return true;
     } else {
       this.client
-        .create("ar_room", {name: this.userName.value})
+        .create("ar_room", { name: this.userName.value })
         .then((room) => {
           console.log(room);
 
@@ -57,21 +76,21 @@ export default class Client {
 
   initializeRoom(room) {
     this.createJoinRoomWrapper.style.display = "none";
-    this.roomInfo.innerHTML = room.id;
-    
+    this.roomCodeDisplay.innerText = room.id;
+    this.goBack.classList.remove("d-none");
+    this.roomInfo.classList.remove("d-none");
+
     this.myKey = room.sessionId;
 
     room.state.collaborators.onAdd = function (collaborator, key) {
-      if(key !== room.sessionId)
+      if (key !== room.sessionId)
         AddCollaborator(key, collaborator.modelIndex, collaborator.userName);
     }
 
     room.state.collaborators.onRemove = function (collaborator, key) {
-      if(key !== room.sessionId)
+      if (key !== room.sessionId)
         RemoveCollaborator(key);
     }
 
   }
-  
-
 }
--- src/main.js
+++ src/main.js
@@ -10,9 +10,61 @@ client.createBtn.onclick = async function (e) {
   }
 };
 
-client.joinBtn.onclick = async function (e) {
+// client.joinBtn.onclick = async function (e) {
+//   if (await client.joinRoom(client.codeInputVal)) {
+//     ARInit();
+//     ARAnimate();
+//   }
+// };
+
+client.backBtn.onclick = function (e) {
+  window.location.reload();
+};
+
+
+var EXISTING_ROOMS_DATATABLE;
+$(document).ready(function () {
+  EXISTING_ROOMS_DATATABLE = $('#ExistingRoomsTable').DataTable({
+    dom: "",
+    responsive: true,
+    autoWidth: false,
+    paging: false,
+    ordering: false,
+    language: {
+      "emptyTable": "No Rooms Available"
+    },
+    columnDefs: [
+      {
+        targets: [2],
+        width: "50px",
+        render: function (data, type, row, meta) {
+          return '<button type="button" class="btn btn-secondary btn-sm btnJoinRoom" data-room-code="' + data + '"><span class="bi-people"></span> Join Room</button>';
+        }
+      }
+    ]
+  });
+
+  setExistingRoomTable();
+});
+
+
+async function setExistingRoomTable() {
+
+  var roomInfo = await client.getRooms();
+
+  roomInfo.forEach((info) => {
+    EXISTING_ROOMS_DATATABLE.row.add([info.roomId, info.clients, info.roomId]).draw(false);
+  });
+}
+
+$(document).on('click', '.btnJoinRoom', function () {
+  client.codeInput = $(this).data('room-code');
+  joinRoom();
+});
+
+async function joinRoom(roomCode) {
   if (await client.joinRoom(client.codeInputVal)) {
     ARInit();
     ARAnimate();
   }
-};
+}
